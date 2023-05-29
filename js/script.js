61c21633a367033d26ee97bddb407aeeb3317ef3
Denis12312323 <uc4iha.sora@gmail.com>
2023-05-29 20:43:20 +0300

    js

--- js/script.js
+++ js/script.js
@@ -32,22 +32,65 @@ if ($('.slider__body').length > 0) {
 }
 // =======================================
 
-$(function (e) {
   let header = $('#header');
   let intro = $('#intro');
   let introH = intro.innerHeight();
   let scrollPos = $(window).scrollTop();
 
-  $(window).on('scroll load resize', function () {
-    let introH = intro.innerHeight();
-    scrollPos = $(this).scrollTop()
+  checkScroll(scrollPos, introH);
 
-    if (scrollPos > introH) {
-      header.addClass('fixed');
-    } else {
-      header.removeClass('fixed');
-    }
+  $(window).on('scroll resize', function () {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+
+    checkScroll(scrollPos, introH);
   });
+
+function checkScroll(scrollPos, introH) {
+  if (scrollPos > introH) {
+    header.addClass('fixed');
+  } else {
+    header.removeClass('fixed');
+  }
+}
+
+// Burger
+
+let nav = $('#nav')
+
+$("#burger").on('click', function (e) {
+  e.preventDefault();
+
+  nav.toggleClass('show');
+})
+
+// ======================================= Scroll
+
+$('[data-scroll]').on('click', function (e) {
+  e.preventDefault();
+
+  let elementId = $(this).data('scroll');
+  let elementOffset = $(elementId).offset().top;
+
+  nav.removeClass('show')
+
+  $('html, body').animate({
+    scrollTop: elementOffset - 75
+
+  }, 700);
 });
 
-// =======================================
+
+// ======================================= Slick-Slider
+
+let slider = $('#testimonialsSlider');
+
+slider.slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  arrows: false,
+  dots: true
+});
